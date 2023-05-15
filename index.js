a8b1a9b68b49b9aec15015cfef88f287defc4e42
Alonso Gomez <a.gomez@gmail.com>
2023-05-14 20:31:15 -0400

    Actualizacion de funcionalidades, localstorage, entre otros

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 //VARIABLES
 let carrito = [];
-var a = 2; 
+var id=0;
+
+//FUNCION AGREGAR LOCALSTORAGE
+const clickAgregar = async (event) => {
+  const tarjeta = event.target;
+  const item = tarjeta.closest('.card-body');
+  const id_producto = item.querySelector('.id-product').textContent;
+  try {
+    const respuesta1 = await fetch('http://localhost:3000/products/'.concat(id_producto));
+    if (respuesta1.status === 200) {
+      const datos = await respuesta1.json();
+      let datosArray = JSON.parse(localStorage.getItem('carrito')) || [];
+      datosArray.push(datos);
+      let datosJson = JSON.stringify(datosArray);
+      localStorage.setItem('carrito', datosJson);
+      mostrarProductosEnCarrito(datosArray);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+function mostrarProductosEnCarrito(datosArray) {
+  let cartas = '';
+  datosArray.forEach(element => {
+    let title = element.name;
+    let precio = element.price;
+    let image = element.image;
+    cartas += `
+      <div class="cart-product">
+        <div class="info-cart-product">
+          <span class="count-cart-product">1</span>
+          <img src="${image}" alt="" style="width: 80px; height: 80px;">
+          <p class="title-cart-product">${title}</p>
+          <span id="price-cart-product">${precio}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-close">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>                    
+        </div>
+      </div>`;
+  });
+  let divFinal =`
+    <div class="cart-total">
+      <h3>Total:</h3>
+      <span class="total-pagar"></span>
+    </div>` 
+  ;
+  $('#cart-cards').html(cartas);
+  $('#cart-cards').append(divFinal);
+
+}
 //FUNCION PARA OBTENER TODOS LOS DATOS
 const loadAllProducts = async() => {
   //VERIFICACION
@@ -24,13 +73,22 @@ const loadAllProducts = async() => {
                   <div class="card-body">
                       <h5 class="card-title">${title}</h5>
                       <p class="card-text">${description}</p>
-                      <a href="#" id="${id}" class="btn btn-success btn-agregar">Agregar al carrito</a>
+                      <a href="#" id="btn-agregar" class="btn btn-success btn-agregar">Agregar al carrito</a>
+                      <div class="id-product" style="display:none;">${id}</div>
                   </div>
               </div>
           </div>`  
         ;
-        $('#info-cards-all').html(cartas);
       });
+
+      $('#info-cards-all').html(cartas);
+      //AGREGAR ACCION BTN A TODOS LOS PRODUCTOS GENERADOS
+      const btnAgregar = document.querySelectorAll('#info-cards-all #btn-agregar');
+      btnAgregar.forEach(product => {
+        product.removeEventListener('click', clickAgregar);
+        product.addEventListener('click', clickAgregar);
+      });
+      
     }else if (respuesta.status===401) {
       console.log('La url API Invalida!');
     }else if (respuesta.status===404){
@@ -41,7 +99,7 @@ const loadAllProducts = async() => {
   }catch(error){
     console.log(error);
   }
-};
+}
 //FUNCION PARA CARGAR PRODUCTOS CINNAMON ROLL
 const loadCinnamons = async() => {
   //VERIFICACION
@@ -72,29 +130,17 @@ const loadCinnamons = async() => {
                 </div>
             </div>`  
           ;
-          $('#info-cards-cinnamon').html(cartas);
+          
       });
-      
-      const btnAgregar = document.querySelectorAll('#btn-agregar')
-      btnAgregar.forEach(product =>{
-        product.addEventListener('click',clickAgregar)
-      })
 
-      function clickAgregar(event){
-        const tarjeta = event.target;
-        const item = tarjeta.closest('.card-body');
-        const id_producto = item.querySelector('.id-product').textContent;
-        fetch('http://localhost:3000/products/'.concat(id_producto))
-        .then(async (respuesta1) => {
-          if (respuesta1.status === 200) {
-            const datos = await respuesta1.json();
-            carrito.push(datos);
-            console.log(carrito);
-            console.log(datos);
-          }
-        })
-      }
-      
+      $('#info-cards-cinnamon').html(cartas);
+
+      const btnAgregar = document.querySelectorAll('#info-cards-cinnamon #btn-agregar');
+      btnAgregar.forEach(product => {
+        product.removeEventListener('click', clickAgregar);
+        product.addEventListener('click', clickAgregar);
+      });
+
     }else if (respuesta.status===401) {
       console.log('La url API Invalida!');
     }else if (respuesta.status===404){
@@ -115,28 +161,35 @@ const loadTortas = async() => {
     if(respuesta.status===200){
       const datos = await respuesta.json();
       //FOREACH PARA RECORRER EL ARRAY DEL API
+      const cards = document.querySelectorAll('.card');
       let cartas = '';
       datos.forEach((producto, i) => {
-        const image = producto.image;
-        image.src = producto.image;
-        const title = producto.name;
-        const description = producto.description;
-        cartas +=  `
-          <div class="col-12 col-md-3 col-sm-6 col-lg-3 mb-4">
-              <div class="card">
-                  <img src="${image}" id="card-img" alt="...">
-                  <div class="card-body">
-                      <h5 class="card-title">${title}</h5>
-                      <p class="card-text">${description}</p>
-                      <a href="#" id="btn-agregar" class="btn btn-success">Agregar al carrito</a>
-                  </div>
-              </div>
-          </div>`  
-        ;
-        $('#info-cards-tortas').html(cartas);
-    
+          const image = producto.image;
+          image.src = producto.image;
+          const title = producto.name;
+          const description = producto.description;
+          const id = producto.id;
+          cartas +=  `
+            <div class="col-12 col-md-3 col-sm-6 col-lg-3 mb-4">
+                <div class="card">
+                    <img src="${image}" id="card-img" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${description}</p>
+                        <a href="#" id="btn-agregar" class="btn btn-success">Agregar al carrito</a>
+                        <div class="id-product" style="display:none;">${id}</div>
+                    </div>
+                </div>
+            </div>`  
+          ;
       });
-      
+      $('#info-cards-tortas').html(cartas);
+
+      const btnAgregar = document.querySelectorAll('#info-cards-tortas #btn-agregar');
+      btnAgregar.forEach(product => {
+        product.removeEventListener('click', clickAgregar);
+        product.addEventListener('click', clickAgregar);
+      });
     }else if (respuesta.status===401) {
       console.log('La url API Invalida!');
     }else if (respuesta.status===404){
@@ -163,6 +216,7 @@ const loadCupcakes = async() => {
         image.src = producto.image;
         const title = producto.name;
         const description = producto.description;
+        const id = producto.id;
         cartas +=  `
           <div class="col-12 col-md-3 col-sm-6 col-lg-3 mb-4">
               <div class="card">
@@ -171,12 +225,20 @@ const loadCupcakes = async() => {
                       <h5 class="card-title">${title}</h5>
                       <p class="card-text">${description}</p>
                       <a href="#" id="btn-agregar" class="btn btn-success">Agregar al carrito</a>
+                      <div class="id-product" style="display:none;">${id}</div>
                   </div>
               </div>
           </div>`  
         ;
-        $('#info-cards-cupcakes').html(cartas);
       });
+      $('#info-cards-cupcakes').html(cartas);
+
+      const btnAgregar = document.querySelectorAll('#info-cards-cupcakes #btn-agregar');
+      btnAgregar.forEach(product => {
+        product.removeEventListener('click', clickAgregar);
+        product.addEventListener('click', clickAgregar);
+      });
+
     }else if (respuesta.status===401) {
       console.log('La url API Invalida!');
     }else if (respuesta.status===404){
@@ -212,12 +274,21 @@ const loadPie = async() => {
                         <h5 class="card-title">${title}</h5>
                         <p class="card-text">${description}</p>
                         <a href="#" id="btn-agregar" class="btn btn-success">Agregar al carrito</a>
+                        <div class="id-product" style="display:none;">${id}</div>
                     </div>
                 </div>
             </div>`  
         ;
-        $('#info-cards-pie').html(cartas);
       });
+      $('#info-cards-pie').html(cartas);
+
+      const btnAgregar = document.querySelectorAll('#info-cards-pie #btn-agregar');
+      btnAgregar.forEach(product => {
+        product.removeEventListener('click', clickAgregar);
+        product.addEventListener('click', clickAgregar);
+      });
+    
+
     }else if (respuesta.status===401) {
       console.log('La url API Invalida!');
     }else if (respuesta.status===404){
@@ -229,56 +300,17 @@ const loadPie = async() => {
     console.log(error);
   }
 }
-
-const loadProduct = async()=> {
-  try{
-    const respuesta = await fetch('http://localhost:3000/products/1');
-
-
-  }catch(error){
-    console.log(error);
-  }
-}
-
 //FUNCIONES PARA CARGAR PRODUCTOS
-
 loadAllProducts();
 loadCupcakes();
 loadCinnamons();
 loadTortas();
 loadPie();
-
 //FUNCION CARRITO COMPRA
-
 /*MOSTRAR CARRITO*/
-document.addEventListener('DOMContentLoaded', function() {
-  const btnCart = document.querySelector('.icon-cart');
-  const containerCartProducts = document.querySelector('.container-cart-products');
-  btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('hidden-cart');
-    let cartas='';
-    carrito.forEach((producto, i) => {
-      const image = producto.image;
-      image.src = producto.image;
-      const title = producto.name;
-      const precio = producto.price;
-      const id = producto.id;
-      cartas +=  `
-      <div class="cart-product">
-        <div class="info-cart-product">
-          <span class="count-cart-product">1</span>
-          <img src="${image}" alt="" style="width: 80px; height: 80px;">
-          <p class="title-cart-product">${title}</p>
-          <span id="price-cart-product">${precio}</span>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-close">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>                    
-        </div>
-      </div>`  
-      ;
-      $('#cart-cards').html(cartas);
-    })
-  });
-});
 
-
+const btnCart = document.querySelector('.icon-cart');
+const containerCartProducts = document.querySelector('.container-cart-products');
+btnCart.addEventListener('click', () => {
+  containerCartProducts.classList.toggle('hidden-cart');
+})
