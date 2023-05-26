a1009855484e39186035a522e53f7018963e86ed
brayanMoreno <karathril@gmail.com>
2023-05-26 17:14:43 -0400

    actualización

--- Ingreso.js
+++ Ingreso.js
@@ -1,24 +1,23 @@
-$("#login").click(function( ){
-  $.getJSON('Content-jsonServer/db.json', function(data) {
-    var ingreso_email =$("#ingreso_email").val()
-    var ingreso_password =$("#ingreso_password").val()
-    var productos = data.users;
-    
+$("#login").click(function() {
+  var ingreso_email = $("#ingreso_email").val();
+  var ingreso_password = $("#ingreso_password").val();
+  
+  $.getJSON('http://localhost:3000/users', function(data) {
     function verificarUsuario(ingreso_email, ingreso_password, productos) {
       for (let i = 0; i < productos.length; i++) {
         if (productos[i].email === ingreso_email && productos[i].password === ingreso_password) {
           return true;
-        };
-      };
+        }
+      }
       return false;
-    };
+    }
     
-    if (verificarUsuario(ingreso_email, ingreso_password, productos)) {
+    if (verificarUsuario(ingreso_email, ingreso_password, data)) {
       alert("correcto");
       window.location.href = "index.html";
-    }else{
+    } else {
       alert("malo");
-    };   
-  });    
+    }
+  });
 });
 
--- Registro.js
+++ Registro.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
     var v_Apellido_Paterno =$("#Apellido_Paterno").val()
 
     if (/\s/g.test(v_nombre)==true || /\d/.test(v_nombre)==true || v_nombre=="" || /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(v_nombre)){ 
-        alert("error con el nombre");
+        alert("Nombre invalido");
     }else if (/\s/g.test(v_Apellido_Paterno)==true || /\d/.test(v_Apellido_Paterno)==true || v_Apellido_Paterno=="" || /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(v_Apellido_Paterno)){ 
-        alert("error con el apellido");
+        alert("Apellido invalido");
     }else if (/\s/g.test(v_email)==true || v_email=="" || /[ `!#$%^&*()+\-=\[\]{};':"\\|,<>\/?~]/.test(v_email) || false==(v_email.indexOf("@gmail.com") !== -1)){ 
-        alert("error con Correo electronico");
+        alert("El correo electronico solo acepta '. y _' y solo debe ser '@gmail.com'");
     }else if (v_compro_email!==v_email){
         alert("Correo electronico no coincide");
-    }else if (/\s/g.test(v_password)==true || /\d/.test(v_password)==false || v_password=="" || false==/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(v_password)){ 
-        alert("la contraseña necesita minimo un número y un caracter especial");
+    }else if (/\s/g.test(v_password)==true || /\d/.test(v_password)==false || v_password=="" || false==/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(v_password) || v_password.length < 5){ 
+        alert("la contraseña necesita minimo un número, un caracter especial y 6 caracteres mínimo");
     }else if (v_compro_password!==v_password){
         alert("contraseña no coincide");
     }else{
