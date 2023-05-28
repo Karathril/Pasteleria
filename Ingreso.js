f3a3d01d803c70e50c0560c9324b4a4a5aa399be
brayanMoreno <karathril@gmail.com>
2023-05-28 15:57:44 -0400

    actualización

---
+++ Ingreso.js
@@ -0,0 +1,32 @@
+$("#login").click(function() {
+  var ingreso_email = $("#ingreso_email").val();
+  var ingreso_password = $("#ingreso_password").val();
+  
+  
+  $.getJSON('http://localhost:3000/users', function(data) {
+    function verificarUsuario(ingreso_email, ingreso_password, productos) {
+      for (let i = 0; i < productos.length; i++) {
+        if (productos[i].email === ingreso_email && productos[i].password === ingreso_password) {
+          return true;
+        }
+      }
+      return false;
+    }
+    
+    if (verificarUsuario(ingreso_email, ingreso_password, data)) {
+      alert("correcto");
+      localStorage.setItem('sesionIniciada', ingreso_email);
+      localStorage.setItem('verificacion', 'si');
+      window.location.href = "index.html";
+    } else {
+      alert("malo");
+      localStorage.setItem('verificacion', 'no');
+
+    }
+  });
+});
+
+$("#regresar").click(function() {
+  localStorage.setItem('verificacion', 'no');
+  window.location.href = "index.html";
+});
